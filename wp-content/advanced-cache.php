ebe48e08a791c7af69b6fe54e92cbafffd2b9468
fiorellasaro <fiorellasaro27@gmail.com>
2020-11-04 12:57:48 -0500

    divi and some plugins

---
+++ wp-content/advanced-cache.php
@@ -0,0 +1,10 @@
+<?php
+/* PageSpeed Ninja Caching */
+defined('ABSPATH') || die();
+define('PAGESPEEDNINJA_CACHE_DIR', 'C:\xampp\htdocs\Igualab\wp-content\plugins\psn-pagespeed-ninja/cache');
+define('PAGESPEEDNINJA_CACHE_PLUGIN', 'C:\xampp\htdocs\Igualab\wp-content\plugins\psn-pagespeed-ninja');
+define('PAGESPEEDNINJA_CACHE_RESSDIR', 'C:\xampp\htdocs\Igualab\wp-content\plugins\psn-pagespeed-ninja/ress');
+define('PAGESPEEDNINJA_CACHE_DEVICEDEPENDENT', true);
+define('PAGESPEEDNINJA_CACHE_TTL', 86400);
+define('PAGESPEEDNINJA_CACHE_GZIP', 1);
+include 'C:\xampp\htdocs\Igualab\wp-content\plugins\psn-pagespeed-ninja/public/advanced-cache.php';
